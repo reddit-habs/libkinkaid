524d9dde8ec27cc19179d641126c619970684657
Simon Bernier St-Pierre <git@sbstp.ca>
2019-10-19 22:58:57 -0400

    init www

---
+++ www/web.py
@@ -0,0 +1,74 @@
+import cgi
+
+from bottle import get, post, request, run
+
+import libkinkaid
+
+
+@get("/")
+def index():
+    return """\
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Kincode translator</title>
+    <style>
+        textarea {
+            resize: vertical;
+        }
+    </style>
+</head>
+<body>
+    <div>
+        <h2>Decode</h2>
+        <form method="post" action="/decode">
+            <textarea name="source" cols="100" rows="5"></textarea>
+            <br>
+            <button type="submit">Decode</button>
+        </form>
+    </div>
+    <div>
+        <h2>Encode</h2>
+        <form method="post" action="/encode">
+            <textarea name="source" cols="100" rows="5"></textarea>
+            <br>
+            <button type="submit">Encode</button>
+        </form>
+    </div>
+</body>
+</html>
+"""
+
+def generate_result_page(result):
+    #result = cgi.escape(result)
+    return f"""\
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Kincode translator result</title>
+</head>
+<body>
+    <a href="/">Back to home page</a>
+    <hr>
+    {result}
+</body>
+</html>
+"""
+
+@post("/decode")
+def decode():
+    source = request.forms.source
+    result = libkinkaid.decode(source)
+    return generate_result_page(result)
+
+
+@post("/encode")
+def encode():
+    source = request.forms.source
+    result = libkinkaid.encode(source)
+    return generate_result_page(result)
+
+
+run(host='localhost', port=8080, reloader=True)
